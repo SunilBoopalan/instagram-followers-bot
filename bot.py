d01c2347231dbf1a210de4418bb60d455d9be3e8
Sunil Boopalan <96052242+SunilBoopalan@users.noreply.github.com>
2021-12-17 14:22:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ from random import randint
 
 while(1):
 	try:
-		os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o follow-tag -t followforfollow ")
-		secs = randint(int(sys.argv[1]), int(sys.argv[2]))
-		time.sleep(secs)
-		os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o super-unfollow")
-		secs = randint(int(sys.argv[1]), int(sys.argv[2]))
-		time.sleep(secs)
+	       os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o follow-tag -t followforfollow ")
+	       secs = randint(int(sys.argv[1]), int(sys.argv[2]))
+	       time.sleep(secs)
+               os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o super-unfollow")
+	       secs = randint(int(sys.argv[1]), int(sys.argv[2]))
+	       time.sleep(secs)
 	except:
-		pass
+	       pass
 
